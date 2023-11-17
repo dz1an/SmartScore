c74ba94722625c8e982e53d2da7a369177455332
kent <dzian2k17@gmail.com>
2023-11-17 18:53:08 +0800

    good

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SmartScope</title>
+    <script src="https://unpkg.com/vue@2.6.14/dist/vue.js"></script>
 </head>
 <body>
     <h1>SmartScope</h1>
     
-    <!-- Form and PHP processing code for image upload -->
+    <div id='app'>
+        
+    </div>
     
+    <!-- Form and PHP processing code for image upload -->
+
+    <script src="js/app.js"></script>
 </body>
+</html>
+
+
+<!-- npm install -g @vue/cli -->
